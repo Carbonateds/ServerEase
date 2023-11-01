5d557c9d97975a7900e6e906e64fd9b6ce74d386
Carbonate <90555321+Carbonateds@users.noreply.github.com>
2023-11-02 01:06:03 +0800

    feat: add windows ui program

---
+++ src/win_ui/App.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ServerEase
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ExportInternalDLLFile("libServerEase.dll");
+
+            Library.Load(); //Load the core library into memory, so the functions can be called.
+
+            Trace.Write("Current Library Version: " + Marshal.PtrToStringAnsi(Library.GetLibraryVersion()));
+        }
+
+        /// <summary>
+        /// Export internal DLL files into local temporary folder.
+        /// </summary>
+        /// <param name="dllName">File name of the DLL file.</param>
+        /// <exception cref="FileLoadException"></exception>
+        private void ExportInternalDLLFile(string dllName)
+        {
+            string dllPath = Path.Combine(Path.GetTempPath(), dllName);
+            if(!File.Exists(dllPath))
+            {
+                using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ServerEase." + dllName))
+                {
+                    if (stream != null)
+                    {
+                        using (FileStream fileStream = File.Open(dllPath, FileMode.Create))
+                        {
+                            stream.CopyTo(fileStream);
+                        }
+                    }
+                }
+            }
+            if(!File.Exists(dllPath))
+                throw new FileLoadException();
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            Library.Free();
+        }
+    }
+}
---
+++ src/win_ui/Library.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerEase
+{
+    internal class Library
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr LoadLibrary(string lpLibFileName);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+        [DllImport("kernel32.dll")]
+        private static extern bool FreeLibrary(IntPtr hLibModule);
+
+        public delegate IntPtr DGetLibraryVersion();
+
+        public static DGetLibraryVersion GetLibraryVersion;
+
+        private static IntPtr LibraryModule;
+
+        /// <summary>
+        /// Instruct the program to load the core library into memory using Win32 APIs.
+        /// </summary>
+        public static void Load()
+        {
+            LibraryModule = LoadLibrary(Path.Combine(Path.GetTempPath(), "libServerEase.dll"));
+            GetLibraryVersion = Marshal.GetDelegateForFunctionPointer<DGetLibraryVersion>(GetProcAddress(LibraryModule, "GetLibraryVersion"));
+        }
+
+        /// <summary>
+        /// Instrust the program to unload the core library from memory.
+        /// </summary>
+        public static void Free()
+        {
+            FreeLibrary(LibraryModule);
+        }
+    }
+}
